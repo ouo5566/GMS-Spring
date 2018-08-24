86ec21ceeb15d129792ba7d281bdbeb1646ee256
ouo5566 <ouo5566@gmail.com>
2018-08-24 15:12:23 +0900

    step_4 first commit // mybatis 설정 before

--- src/main/java/com/gms/web/controller/MemberController.java
+++ src/main/java/com/gms/web/controller/MemberController.java
@@ -1,11 +1,8 @@
 package com.gms.web.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.gms.web.domain.MemberDTO;
 
 @Controller
 @RequestMapping("/member")
