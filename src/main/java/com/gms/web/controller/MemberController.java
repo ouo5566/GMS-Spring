ba3c84522a79d6ce924e77bc9ec6d2804587128f
ouo5566 <ouo5566@gmail.com>
2018-08-27 18:14:29 +0900

    mybatis 설정 after...

--- src/main/java/com/gms/web/controller/MemberController.java
+++ src/main/java/com/gms/web/controller/MemberController.java
@@ -1,13 +1,21 @@
 package com.gms.web.controller;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gms.web.domain.MemberDTO;
+import com.gms.web.service.MemberService;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 	static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	@Autowired MemberService memberService;
 	@RequestMapping("/add")
 	public void add() {
 		
@@ -39,6 +47,10 @@ public class MemberController {
 	@RequestMapping("/login")
 	public String login() {
 		logger.info("Member Controller :: login()");
+		Map<String, String> p = new HashMap<>();
+		p.put("memberId", "HJ34");
+		MemberDTO m = memberService.retrieve(p);
+		logger.info("Member Controller :: login() :: getName() :: "+ m.getName());
 		return "login_success";
 	}
 	@RequestMapping("/logout")
